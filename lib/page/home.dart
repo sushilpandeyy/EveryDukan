b394ce36c8a7efb12ed6eb153b8190ec97dd2749
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-01-04 23:41:50 +0530

    SlideShow Added

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../component/bottom.dart';
 import '../component/header.dart';
+import '../component/slideshow.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,9 +28,21 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+     final List<String> imageUrls = [
+      'https://asset22.ckassets.com/resources/image/staticpage_images/mCaffeine-Desktop%204-1735796309.png',
+      'https://asset22.ckassets.com/resources/image/staticpage_images/Store-Desktop-Ajio-1735796225.png',
+      'https://asset22.ckassets.com/resources/image/staticpage_images/Store-Desktop-Myntra-min-1735884857.png',
+      'https://asset22.ckassets.com/resources/image/staticpage_images/Background-min-1728982477.png',
+    ];
+
     return Scaffold(
       appBar: const Header(), // Call the custom Header component here
-      body: _pages[_currentIndex],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: BannerSlideshow(imageUrls: imageUrls),
+        ),
+      ),
       bottomNavigationBar: CustomBottomNavigation(
         currentIndex: _currentIndex,
         onTap: _onTabTapped,
