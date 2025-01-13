d22b1d3938eccdffab6d8051988f32b3416c8fae
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-01-13 13:25:16 +0530

    Corected

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'dart:io' show Platform;
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
  import './page/home.dart';
@@ -7,11 +10,36 @@ import 'page/coupon.dart';
 import 'page/deals.dart';
 
 void main() async {
- WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  // Request notification permissions
+  final notificationSettings = await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+    provisional: true,
+  );
+
+  // Get FCM token for this device
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM Token: $fcmToken');
+
+  // Only try to get APNS token on iOS
+  if (Platform.isIOS) {
+    final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+    if (apnsToken != null) {
+      print('Got APNS token: $apnsToken');
+    } else {
+      print('APNS token not available yet');
+    }
+  }
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
